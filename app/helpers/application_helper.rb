d06b392d104b3dd062913819c8c2e250c93741bd
Camila Bignone <camilabignone@gmail.com>
2017-11-24 11:30:20 -0300

    add styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,17 +2,18 @@ module ApplicationHelper
   def li_login
     if user_signed_in?
       content_tag(:li, class: 'nav-item') do
-        link_to('Logout', destroy_user_session_path, method: :delete, class: 'nav-link')
+        link_to('Logout', destroy_user_session_path, method: :delete, class: 'btn btn-outline-secondary my-2 my-sm-0')
       end
     else
       capture do
         content_tag(:li, class: 'nav-item') do
-         concat  link_to('Register', new_user_registration_path, class: 'nav-link')
+         concat  link_to('register', new_user_registration_path, class: 'btn btn-outline-secondary my-2 my-sm-0')
         end +
         content_tag(:li, class: 'nav-item') do
-         concat  link_to('Login', new_user_session_path, class: 'nav-link')
+         concat  link_to('Login', new_user_session_path, class: 'btn btn-outline-secondary my-2 my-sm-0')
         end
       end
     end
   end
 end
+
